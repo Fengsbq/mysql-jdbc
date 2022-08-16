0bb73025166a79a6683ae76dbaf4db2c33644b16
Fu Xiantao <18856161393@163.com>
2022-08-16 09:07:56 +0800

    增删改

--- src/main/resources/sql测试.sql
+++ src/main/resources/sql测试.sql
@@ -1,3 +1,17 @@
 -- 测试sql语句
 -- 创建本地仓库 vcs - create git repository
 -- 选择工程文件夹创建本地仓库
+
+
+
+create table tb6(
+                     age int,
+                     gender char(1),
+                     check(age>=7 and age<=60),
+                     check(gender in('M', 'F'))
+);
+
+insert into tb6 values(7, 'M');
+insert into tb6 values(60, 'F');
+insert into tb6 values(61, 'F');
+insert into tb6 valuse(50, 'C');
